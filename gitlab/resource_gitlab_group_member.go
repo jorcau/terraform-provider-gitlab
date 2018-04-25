61d187a9cec742728e61996a752e113a55ebe9a4
Jordan Caussat <jordan.caussat@fr.clara.net>
2018-04-25 11:36:28 +0200

    Rename variables to match Go best practices

--- gitlab/resource_gitlab_group_member.go
+++ gitlab/resource_gitlab_group_member.go
@@ -68,28 +68,28 @@ func resourceGitlabGroupMember() *schema.Resource {
 func resourceGitlabGroupMemberCreate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*gitlab.Client)
 
-	group_id := d.Get("group_id").(string)
-	user_id := d.Get("user_id").(int)
-	access_level_id := accessLevelID[strings.ToLower(d.Get("access_level").(string))]
+	groupID := d.Get("group_id").(string)
+	userID := d.Get("user_id").(int)
+	accessLevelID := accessLevelID[strings.ToLower(d.Get("access_level").(string))]
 
 	options := &gitlab.AddGroupMemberOptions{
-		UserID:      &user_id,
-		AccessLevel: &access_level_id,
+		UserID:      &userID,
+		AccessLevel: &accessLevelID,
 	}
 
 	if exp, ok := d.GetOk("expires_at"); ok {
-		expires_at := exp.(string)
-		options.ExpiresAt = &expires_at
+		expiresAt := exp.(string)
+		options.ExpiresAt = &expiresAt
 	}
 
-	log.Printf("[DEBUG] create gitlab group member %d in %s", user_id, group_id)
+	log.Printf("[DEBUG] create gitlab group member %d in %s", userID, groupID)
 
-	group_member, _, err := client.GroupMembers.AddGroupMember(group_id, options)
+	groupMember, _, err := client.GroupMembers.AddGroupMember(groupID, options)
 	if err != nil {
 		return err
 	}
 
-	d.SetId(fmt.Sprintf("%d", group_member.ID))
+	d.SetId(fmt.Sprintf("%d", groupMember.ID))
 
 	return resourceGitlabGroupMemberRead(d, meta)
 }
@@ -98,32 +98,32 @@ func resourceGitlabGroupMemberRead(d *schema.ResourceData, meta interface{}) err
 	client := meta.(*gitlab.Client)
 	log.Printf("[DEBUG] read gitlab group member %s", d.Id())
 
-	group_id := d.Get("group_id").(string)
-	user_id := d.Get("user_id").(int)
+	groupID := d.Get("group_id").(string)
+	userID := d.Get("user_id").(int)
 
-	group_member, resp, err := client.GroupMembers.GetGroupMember(group_id, user_id)
+	groupMember, resp, err := client.GroupMembers.GetGroupMember(groupID, userID)
 	if err != nil {
 		if resp.StatusCode == 404 {
-			log.Printf("[WARN] removing group member %s in %s from state because it no longer exists in gitlab", d.Id(), group_id)
+			log.Printf("[WARN] removing group member %s in %s from state because it no longer exists in gitlab", d.Id(), groupID)
 			d.SetId("")
 			return nil
 		}
 		return err
 	}
 
-	d.Set("access_level", accessLevel[group_member.AccessLevel])
-	if group_member.ExpiresAt != nil {
-		d.Set("expires_at", group_member.ExpiresAt.String())
+	d.Set("access_level", accessLevel[groupMember.AccessLevel])
+	if groupMember.ExpiresAt != nil {
+		d.Set("expires_at", groupMember.ExpiresAt.String())
 	}
-	if group_member.CreatedAt != nil {
-		d.Set("created_at", group_member.CreatedAt.String())
+	if groupMember.CreatedAt != nil {
+		d.Set("created_at", groupMember.CreatedAt.String())
 	}
-	if group_member.Email != "" {
-		d.Set("email", group_member.Email)
+	if groupMember.Email != "" {
+		d.Set("email", groupMember.Email)
 	}
-	d.Set("username", group_member.Username)
-	d.Set("name", group_member.Name)
-	d.Set("state", group_member.State)
+	d.Set("username", groupMember.Username)
+	d.Set("name", groupMember.Name)
+	d.Set("state", groupMember.State)
 
 	return nil
 }
@@ -131,22 +131,22 @@ func resourceGitlabGroupMemberRead(d *schema.ResourceData, meta interface{}) err
 func resourceGitlabGroupMemberUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*gitlab.Client)
 
-	group_id := d.Get("group_id").(string)
-	user_id := d.Get("user_id").(int)
-	access_level_id := accessLevelID[strings.ToLower(d.Get("access_level").(string))]
+	groupID := d.Get("group_id").(string)
+	userID := d.Get("user_id").(int)
+	accessLevelID := accessLevelID[strings.ToLower(d.Get("access_level").(string))]
 
 	options := &gitlab.EditGroupMemberOptions{
-		AccessLevel: &access_level_id,
+		AccessLevel: &accessLevelID,
 	}
 
 	if exp, ok := d.GetOk("expires_at"); ok {
-		expires_at := exp.(string)
-		options.ExpiresAt = &expires_at
+		expiresAt := exp.(string)
+		options.ExpiresAt = &expiresAt
 	}
 
-	log.Printf("[DEBUG] update gitlab group member %s in %s", d.Id(), group_id)
+	log.Printf("[DEBUG] update gitlab group member %s in %s", d.Id(), groupID)
 
-	_, _, err := client.GroupMembers.EditGroupMember(group_id, user_id, options)
+	_, _, err := client.GroupMembers.EditGroupMember(groupID, userID, options)
 	if err != nil {
 		return err
 	}
@@ -157,12 +157,12 @@ func resourceGitlabGroupMemberUpdate(d *schema.ResourceData, meta interface{}) e
 func resourceGitlabGroupMemberDelete(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*gitlab.Client)
 
-	group_id := d.Get("group_id").(string)
-	user_id := d.Get("user_id").(int)
+	groupID := d.Get("group_id").(string)
+	userID := d.Get("user_id").(int)
 
-	log.Printf("[DEBUG] delete gitlab group member %s from %s", d.Id(), group_id)
+	log.Printf("[DEBUG] delete gitlab group member %s from %s", d.Id(), groupID)
 
-	_, err := client.GroupMembers.RemoveGroupMember(group_id, user_id)
+	_, err := client.GroupMembers.RemoveGroupMember(groupID, userID)
 	if err != nil {
 		return err
 	}
@@ -175,17 +175,17 @@ func resourceGitlabGroupMemberDelete(d *schema.ResourceData, meta interface{}) e
 func resourceGitlabGroupMemberImport(d *schema.ResourceData, m interface{}) ([]*schema.ResourceData, error) {
 	// Parse ID to get both user_id and group_id - Must be in user_id/group_id format
 	ids := strings.Split(d.Id(), "/")
-	user_id, err := strconv.Atoi(ids[0])
+	userID, err := strconv.Atoi(ids[0])
 	if err != nil {
-		fmt.Errorf("[ERROR] coulnd't parse user_id during import: %v", err)
+		return nil, fmt.Errorf("[ERROR] coulnd't parse user_id during import: %v", err)
 	}
-	group_id := ids[1]
+	groupID := ids[1]
 
-	d.Set("group_id", group_id)
-	d.Set("user_id", user_id)
-	d.SetId(fmt.Sprintf("%d", user_id))
+	d.Set("group_id", groupID)
+	d.Set("user_id", userID)
+	d.SetId(fmt.Sprintf("%d", userID))
 
-	log.Printf("[DEBUG] import gitlab group member %d from %s", user_id, group_id)
+	log.Printf("[DEBUG] import gitlab group member %d from %s", userID, groupID)
 
 	return []*schema.ResourceData{d}, nil
 }
