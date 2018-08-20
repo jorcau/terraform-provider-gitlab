074bece8d694cdcdf34cd4f41948a0a88395308c
Jordan Caussat <jordan.caussat@fr.clara.net>
2018-08-20 17:27:45 +0200

    service: add all options, refactor code for common options and make update function use create since API using same verb (PUT)

--- gitlab/resource_gitlab_service.go
+++ gitlab/resource_gitlab_service.go
@@ -48,46 +48,58 @@ func resourceGitlabService() *schema.Resource {
 			"push_events": {
 				Type:     schema.TypeBool,
 				Computed: true,
+				Optional: true,
 			},
 			"issues_events": {
 				Type:     schema.TypeBool,
 				Computed: true,
+				Optional: true,
 			},
 			"confidential_issues_events": {
 				Type:     schema.TypeBool,
 				Computed: true,
+				Optional: true,
 			},
 			"merge_requests_events": {
 				Type:     schema.TypeBool,
 				Computed: true,
+				Optional: true,
 			},
 			"tag_push_events": {
 				Type:     schema.TypeBool,
 				Computed: true,
+				Optional: true,
 			},
 			"note_events": {
 				Type:     schema.TypeBool,
 				Computed: true,
+				Optional: true,
 			},
 			"confidential_note_events": {
 				Type:     schema.TypeBool,
 				Computed: true,
+				Optional: true,
 			},
 			"pipeline_events": {
 				Type:     schema.TypeBool,
 				Computed: true,
+				Optional: true,
 			},
 			"wiki_page_events": {
 				Type:     schema.TypeBool,
 				Computed: true,
+				Optional: true,
 			},
 			"job_events": {
 				Type:     schema.TypeBool,
 				Computed: true,
 			},
 			"properties": {
-				Type:     schema.TypeMap,
+				Type:     schema.TypeList,
+				MaxItems: 1,
+				Computed: true,
 				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeMap},
 			},
 		},
 	}
@@ -98,19 +110,8 @@ func resourceGitlabServiceCreate(d *schema.ResourceData, meta interface{}) error
 
 	project := d.Get("project").(string)
 	name := d.Get("name").(string)
-	properties := d.Get("properties").(map[string]interface{})
-
-	// gitlabService := gitlab.Service{
-	// 	Active:                   d.Get("active").(bool),
-	// 	PushEvents:               d.Get("pushEvents").(bool),
-	// 	IssuesEvents:             d.Get("issuesEvents").(bool),
-	// 	ConfidentialIssuesEvents: d.Get("confidentialIssuesEvents").(bool),
-	// 	MergeRequestsEvents:      d.Get("mergeRequestsEvents").(bool),
-	// 	TagPushEvents:            d.Get("tagPushEvents").(bool),
-	// 	NoteEvents:               d.Get("noteEvents").(bool),
-	// 	PipelineEvents:           d.Get("pipelineEvents").(bool),
-	// 	JobEvents:                d.Get("jobEvents").(bool),
-	// }
+	properties := d.Get("properties").([]interface{})
+	commonOptions := expandGitlabServiceCommonOptions(d)
 
 	log.Printf("[DEBUG] Create %s Gitlab service", name)
 
@@ -119,6 +120,7 @@ func resourceGitlabServiceCreate(d *schema.ResourceData, meta interface{}) error
 	switch name {
 	case "slack":
 		slackOptions, err := expandSlackOptions(properties)
+		slackOptions.SetGitlabServiceOptions = *commonOptions
 		if err != nil {
 			return err
 		}
@@ -128,7 +130,7 @@ func resourceGitlabServiceCreate(d *schema.ResourceData, meta interface{}) error
 	}
 
 	if setServiceErr != nil {
-		return fmt.Errorf("[ERROR] Couldn't create Gitlab %s service", name)
+		return fmt.Errorf("[ERROR] Couldn't create Gitlab %s service: %s", name, setServiceErr)
 	}
 
 	d.SetId(fmt.Sprintf("%s/%s", project, name))
@@ -178,79 +180,14 @@ func resourceGitlabServiceRead(d *schema.ResourceData, meta interface{}) error {
 	d.Set("note_events", service.NoteEvents)
 	d.Set("pipeline_events", service.PipelineEvents)
 	d.Set("job_events", service.JobEvents)
-	// d.Set("wikiPage_events", service.WikiPageEvents)
-	// d.Set("confidentialNote_events", service.ConfidentialNoteEvents)
+	d.Set("wikiPage_events", service.WikiPageEvents)
+	d.Set("confidentialNote_events", service.ConfidentialNoteEvents)
 
 	return nil
 }
 
 func resourceGitlabServiceUpdate(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*gitlab.Client)
-
-	project := d.Get("project").(string)
-	name := d.Get("name").(string)
-	properties := d.Get("properties").(map[string]interface{})
-
-	// if d.HasChange("title") {
-	// 	title := d.Get("title").(string)
-	// }
-	// if d.HasChange("createdAt") {
-	// 	createdAt := d.Get("createdAt").(string)
-	// }
-	// if d.HasChange("updatedAt") {
-	// 	updatedAt := d.Get("updatedAt").(string)
-	// }
-	// if d.HasChange("active") {
-	// 	active := d.Get("active").(string)
-	// }
-	// if d.HasChange("pushEvents") {
-	// 	pushEvents := d.Get("pushEvents").(string)
-	// }
-	// if d.HasChange("issuesEvents") {
-	// 	issuesEvents := d.Get("issuesEvents").(string)
-	// }
-	// if d.HasChange("confidentialIssuesEvents") {
-	// 	confidentialIssuesEvents := d.Get("confidentialIssuesEvents").(string)
-	// }
-	// if d.HasChange("mergeRequestsEvents") {
-	// 	mergeRequestsEvents := d.Get("mergeRequestsEvents").(string)
-	// }
-	// if d.HasChange("tagPushEvents") {
-	// 	tagPushEvents := d.Get("tagPushEvents").(string)
-	// }
-	// if d.HasChange("noteEvents") {
-	// 	noteEvents := d.Get("noteEvents").(string)
-	// }
-	// if d.HasChange("pipelineEvents") {
-	// 	pipelineEvents := d.Get("pipelineEvents").(string)
-	// }
-	// if d.HasChange("jobEvents") {
-	// 	jobEvents := d.Get("jobEvents").(string)
-	// }
-	// if d.HasChange("properties") {
-	// 	properties := d.Get("properties").(map[string]string)
-	// }
-
-	log.Printf("[DEBUG] update gitlab label %s", d.Id())
-
-	var setServiceErr error
-
-	switch name {
-	case "slack":
-		slackOptions, err := expandSlackOptions(properties)
-		if err != nil {
-			return serviceErrorMsg("properties", name)
-		}
-		_, setServiceErr = client.Services.SetSlackService(project, slackOptions)
-	default:
-		return serviceErrorMsg("name", name)
-	}
-
-	if setServiceErr != nil {
-		return fmt.Errorf("[ERROR] Couldn't create Gitlab service: %s", name)
-	}
-
-	return resourceGitlabServiceRead(d, meta)
+	return resourceGitlabServiceCreate(d, meta)
 }
 
 func resourceGitlabServiceDelete(d *schema.ResourceData, meta interface{}) error {
@@ -272,8 +209,9 @@ func resourceGitlabServiceDelete(d *schema.ResourceData, meta interface{}) error
 	return err
 }
 
-func expandSlackOptions(properties map[string]interface{}) (*gitlab.SetSlackServiceOptions, error) {
-	setSlackServiceOptions := &gitlab.SetSlackServiceOptions{}
+func expandSlackOptions(d []interface{}) (*gitlab.SetSlackServiceOptions, error) {
+	setSlackServiceOptions := gitlab.SetSlackServiceOptions{}
+	properties := d[0].(map[string]interface{})
 
 	// Check required properties are set and valid
 	requiredProperties := []string{"webhook"}
@@ -301,16 +239,100 @@ func expandSlackOptions(properties map[string]interface{}) (*gitlab.SetSlackServ
 	if properties["channel"] != nil {
 		setSlackServiceOptions.Channel = gitlab.String(properties["channel"].(string))
 	}
+	if properties["notify_only_broken_pipelines"] != nil {
+		val, _ := strconv.ParseBool(properties["notify_only_broken_pipelines"].(string))
+		setSlackServiceOptions.NotifyOnlyBrokenPipelines = gitlab.Bool(val)
+	}
+	if properties["notify_only_default_branch"] != nil {
+		val, _ := strconv.ParseBool(properties["notify_only_default_branch"].(string))
+		setSlackServiceOptions.NotifyOnlyDefaultBranch = gitlab.Bool(val)
+	}
+	if properties["push_channel"] != nil {
+		setSlackServiceOptions.PushChannel = gitlab.String(properties["push_channel"].(string))
+	}
+	if properties["issue_channel"] != nil {
+		setSlackServiceOptions.IssueChannel = gitlab.String(properties["issue_channel"].(string))
+	}
+	if properties["confidential_issue_channel"] != nil {
+		setSlackServiceOptions.ConfidentialIssueChannel = gitlab.String(properties["confidential_issue_channel"].(string))
+	}
+	if properties["merge_request_channel"] != nil {
+		setSlackServiceOptions.MergeRequestChannel = gitlab.String(properties["merge_request_channel"].(string))
+	}
+	if properties["note_channel"] != nil {
+		setSlackServiceOptions.NoteChannel = gitlab.String(properties["note_channel"].(string))
+	}
+	// if properties["confidential_note_channel"] != nil {
+	// 	setSlackServiceOptions.ConfidentialNoteChannel = gitlab.String(properties["confidential_note_channel"].(string))
+	// }
+	if properties["tag_push_channel"] != nil {
+		setSlackServiceOptions.TagPushChannel = gitlab.String(properties["tag_push_channel"].(string))
+	}
+	if properties["pipeline_channel"] != nil {
+		setSlackServiceOptions.PipelineChannel = gitlab.String(properties["pipeline_channel"].(string))
+	}
+	if properties["wiki_page_channel"] != nil {
+		setSlackServiceOptions.WikiPageChannel = gitlab.String(properties["wiki_page_channel"].(string))
+	}
 
-	return setSlackServiceOptions, nil
+	return &setSlackServiceOptions, nil
 }
 
-func flattenSlackOptions(service *gitlab.SlackService) map[string]interface{} {
-	values := make(map[string]interface{})
+func flattenSlackOptions(service *gitlab.SlackService) []interface{} {
+	values := []interface{}{}
 
-	values["notifyOnlyBrokenPipelines"] = strconv.FormatBool(service.Properties.NotifyOnlyBrokenPipelines)
+	slackOptions := map[string]interface{}{}
+	slackOptions["webhook"] = service.Properties.WebHook
+	slackOptions["username"] = service.Properties.Username
+	slackOptions["notify_only_broken_pipelines"] = service.Properties.NotifyOnlyDefaultBranch.UnmarshalJSON
+	slackOptions["notify_only_default_branch"] = service.Properties.NotifyOnlyBrokenPipelines.UnmarshalJSON
+	slackOptions["push_channel"] = service.Properties.PushChannel
+	slackOptions["issue_channel"] = service.Properties.IssueChannel
+	slackOptions["confidential_issue_channel"] = service.Properties.ConfidentialIssueChannel
+	slackOptions["merge_request_channel"] = service.Properties.MergeRequestChannel
+	slackOptions["note_channel"] = service.Properties.NoteChannel
+	slackOptions["confidential_note_channel"] = service.Properties.ConfidentialNoteChannel
+	slackOptions["tag_push_channel"] = service.Properties.TagPushChannel
+	slackOptions["pipeline_channel"] = service.Properties.PipelineChannel
+	slackOptions["wiki_page_channel"] = service.Properties.WikiPageChannel
+
+	values = append(values, slackOptions)
 
 	return values
+}
+
+func expandGitlabServiceCommonOptions(d *schema.ResourceData) *gitlab.SetGitlabServiceOptions {
+	commonOptions := gitlab.SetGitlabServiceOptions{}
+
+	if val, ok := d.GetOk("push_events"); ok {
+		commonOptions.PushEvents = gitlab.Bool(val.(bool))
+	}
+	if val, ok := d.GetOk("issues_events"); ok {
+		commonOptions.IssuesEvents = gitlab.Bool(val.(bool))
+	}
+	if val, ok := d.GetOk("confidential_issues_events"); ok {
+		commonOptions.ConfidentialIssuesEvents = gitlab.Bool(val.(bool))
+	}
+	if val, ok := d.GetOk("merge_requests_events"); ok {
+		commonOptions.MergeRequestsEvents = gitlab.Bool(val.(bool))
+	}
+	if val, ok := d.GetOk("tag_push_events"); ok {
+		commonOptions.TagPushEvents = gitlab.Bool(val.(bool))
+	}
+	if val, ok := d.GetOk("note_events"); ok {
+		commonOptions.NoteEvents = gitlab.Bool(val.(bool))
+	}
+	if val, ok := d.GetOk("confidential_note_events"); ok {
+		commonOptions.ConfidentialNoteEvents = gitlab.Bool(val.(bool))
+	}
+	if val, ok := d.GetOk("pipeline_events"); ok {
+		commonOptions.PipelineEvents = gitlab.Bool(val.(bool))
+	}
+	if val, ok := d.GetOk("wiki_page_events"); ok {
+		commonOptions.WikiPageEvents = gitlab.Bool(val.(bool))
+	}
+
+	return &commonOptions
 }
 
 func serviceErrorMsg(errType string, name string) error {
